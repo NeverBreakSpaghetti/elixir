cb459cc99519757d9dc53e802cf2720b4f6e860a
Mauro Magnacavallo <mmagnacavallo@vitesicure.it>
2024-08-01 05:56:11 +0200

    add modules and functions exercises

--- scr/modules_and_functions.exs
+++ scr/modules_and_functions.exs
@@ -9,3 +9,10 @@ defmodule Sum do
   def of(1), do: 1
   def of(n), do: n + of(n-1)
 end
+
+
+# -----ex modulesAndFunctions-5------
+defmodule Gdc do
+  def of(x,0) when is_integer(x), do: x
+  def of(x,y) when is_integer(x) and is_integer(y), do: of(y,rem(x,y))
+end
