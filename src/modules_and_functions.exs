3cf939747b4602552f086128211c691b4a695d43
Mauro Magnacavallo <mmagnacavallo@vitesicure.it>
2024-08-04 01:39:34 +0200

    add modules and functions exercise 6

--- src/modules_and_functions.exs
+++ src/modules_and_functions.exs
@@ -16,3 +16,26 @@ defmodule Gdc do
   def of(x,0) when is_integer(x), do: x
   def of(x,y) when is_integer(x) and is_integer(y), do: of(y,rem(x,y))
 end
+
+# -----ex modulesAndFunctions-6------
+# Chop.guess(273, 1..1000)
+defmodule Chop do
+  def guess(guess, _.._) when is_integer(guess) === false, do: IO.puts("only integers are accepted")
+  def guess(guess, min..max) when (guess in min..max) === false, do: IO.puts("#{guess} not in range")
+  def guess(guess, min..max) when guess in min..max do
+    c = div(max+min,2)
+    IO.puts("is it #{c}")
+    guess(guess, min..max, div(max+min,2))
+  end
+  def guess(guess, _.._, center) when guess === center, do: IO.puts("#{guess}")
+  def guess(guess, min.._, center) when guess <= center do
+    c = div(center+min,2)
+    IO.puts("is it #{c}")
+    guess(guess, min..center, c)
+  end
+  def guess(guess, _..max, center) when guess > center do
+    c = div(max+center,2)
+    IO.puts("is it #{c}")
+    guess(guess, center..max, c)
+  end
+end
