6c51deb60cce8bd471c5dd5b392562697a61cb43
Mauro Magnacavallo <mmagnacavallo@vitesicure.it>
2024-08-08 09:06:16 +0200

    add modules and functions exercise 7 initial

--- src/modules_and_functions.exs
+++ src/modules_and_functions.exs
@@ -39,3 +39,30 @@ defmodule Chop do
     guess(guess, center..max, c)
   end
 end
+
+# -----ex modulesAndFunctions-6------
+#Find the library functions to do the following, and then use each in IEx.
+#(If the word Elixir or Erlang appears at the end of the challenge, then you’ll find the answer in that set of libraries.)
+defmodule Libraries do
+  #Convert a float to a string with two decimal digits. (Erlang)
+  def float_to_string(float) when is_float(float), do: :io.format("~.2f", [float])
+  #def floatToString(float) when is_float(float), do: :erlang.float_to_binary(float, decimals: 2)
+
+  #Get the value of an operating-system environment variable. (Elixir)
+  def get_env_variable(variable), do: System.get_env(variable)
+  def get_system_home(), do: System.user_home()
+
+  #Return the extension component of a file name (so return .exs if given "dave/test.exs"). (Elixir)
+  def get_extension(file_name), do: Path.extname(file_name)
+  #def get_extension(file_name), do: Regex.run(~r{\.\w+}, file_name)
+
+  #Return the process’s current working directory. (Elixir)
+  def get_cwd(), do: File.cwd()
+
+  #Convert a string containing JSON into Elixir data structures. (Just find; don’t install.)
+
+
+  #Execute a command in your operating system’s shell.
+  def execute_shell_echo_command(message), do: System.shell("echo #{message}")
+
+end
