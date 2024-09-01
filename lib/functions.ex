f2a083b555aa01f5921619e2f119f53c0d91e92a
Mauro Magnacavallo <mmagnacavallo@vitesicure.it>
2024-09-01 12:06:03 +0200

    refactor functions 1

--- lib/functions.ex
+++ lib/functions.ex
@@ -1,15 +1,9 @@
 defmodule Functions do
 
-  def list_concat(list1, list2) do
-    list1 ++ list2
-  end
+  def list_concat(list1, list2), do: list1 ++ list2
 
-  def sum(n1, n2, n3) do
-    n1 + n2 + n3
-  end
+  def sum(n1, n2, n3), do: n1 + n2 + n3
 
-  def pair_tuple_to_list({a,b}) do
-    [a,b]
-  end
+  def pair_tuple_to_list({a,b}), do: [a,b]
 
 end
