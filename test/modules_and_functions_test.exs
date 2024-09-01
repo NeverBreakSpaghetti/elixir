6001d0760d4f32f5c78a99b85e154add3c4044af
Mauro Magnacavallo <mmagnacavallo@vitesicure.it>
2024-09-01 14:20:07 +0200

    test quadruple

--- test/modules_and_functions_test.exs
+++ test/modules_and_functions_test.exs
@@ -7,4 +7,11 @@ defmodule ModulesAndFunctionsTest do
       assert triple(3) === 9
     end
   end
+
+  describe "ModulesAndFunctions-3" do
+    test "triple" do
+      assert quadruple(3) === 12
+    end
+  end
+
 end
