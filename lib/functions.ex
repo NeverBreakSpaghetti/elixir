72ee14cff0a5d5ae26171e1d1b314cdba770a742
Mauro Magnacavallo <mmagnacavallo@vitesicure.it>
2024-09-01 12:05:01 +0200

    update functions 1 tests

--- lib/functions.ex
+++ lib/functions.ex
@@ -4,4 +4,12 @@ defmodule Functions do
     list1 ++ list2
   end
 
+  def sum(n1, n2, n3) do
+    n1 + n2 + n3
+  end
+
+  def pair_tuple_to_list({a,b}) do
+    [a,b]
+  end
+
 end
--- test/functions_test.exs
+++ test/functions_test.exs
@@ -2,8 +2,18 @@ defmodule FunctionsTest do
   use ExUnit.Case
   import Functions
 
-  test "list concat" do
-    assert list_concat([:a, :b], [:c, :d]) == [:a, :b, :c, :d]
+  describe "Functions-1" do
+    test "list_concat" do
+      assert list_concat([:a, :b], [:c, :d]) == [:a, :b, :c, :d]
+    end
+
+    test "sum" do
+      assert sum(1,2,3) === 6
+    end
+
+    test "pair_tuple_to_list" do
+      assert pair_tuple_to_list({1234, 5678}) === [1234, 5678]
+    end
   end
 
 end
